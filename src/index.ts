a7769df38e7dbef669a6eb3ea598d9bd01e5eb4a
davin <davinyue@gmail.com>
2024-01-24 15:16:52 +0800

    fix: 修复ide报错未申明类型

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { PayloadAction, EffectTool, Effect, ModelReducer, ModelEffect, ReduxModel, ReduxApp } from './redux/createApp';
 import createApp from './redux/createApp';
 export default {
   createApp: createApp
